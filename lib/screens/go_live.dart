706b2babefcc4d98decadc112f250ed9c01e6ef4
Karan Doshi <karandoshi@karans-air.wifisec.seattleu.edu>
2023-06-19 20:09:49 -0700

    added bottom nav bar

---
+++ lib/screens/go_live.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoLive extends StatefulWidget {
+  const GoLive({super.key});
+
+  @override
+  State<GoLive> createState() => _GoLiveState();
+}
+
+class _GoLiveState extends State<GoLive> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
